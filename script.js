652f807033a6d362c35ab1b80ffbc60c321bddca
Jalyn Campbell <jalyn.campbell@yahoo.com>
2022-08-16 11:33:31 -0400

    moved event listener to after function

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const pause = docuemnt.getElementsByClassName(pausebtn)
 
 
-pausebtn.addEventListener("click", pauseMenu);
+
 
 function pauseMenu () {
 
     window.open();
 
 }
+
+pausebtn.addEventListener("click", pauseMenu);
