0cf26cb18715b3946f653ade972178acc2631ff7
Jalyn Campbell <jalyn.campbell@yahoo.com>
2022-08-25 19:37:34 -0400

    added additional movememt function and comments

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var gameBoard = docuemnt.getElementsById("game-border");
 var gameContext = gameBoard.getContext("webgl", {depth: true, stencil: true})
 var pause = docuemnt.getElementsByClassName("pausebtn");
+/* To move the blocks when user clicks on them */
+var options = $( ".block" ).draggable( "option" );
 
 
 
@@ -13,6 +15,10 @@ var pause = docuemnt.getElementsByClassName("pausebtn");
 
 pausebtn.addEventListener("click", pauseMenu); */
 
+/* To move the blocks when user clicks on them */
+
 $( ".block" ).draggable({
     create: function( event, ui ) {}
   });
+
+  $( ".selector" ).on( "drag", function( event, ui ) {} );
